581226d016f38be10bfef097e4d01f47ead08788
antoniohcarr <ahf0007@alu.medac.es>
2024-05-10 19:04:23 +0200

    Revert "Revert "Update MiPrograma.java""

--- MiPrograma.java
+++ MiPrograma.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class MiPrograma {
 
     public static void main(String[] args) {
-        // Imprime un mensaje en la consola
-        System.out.println("Hola, este es mi primer programa en Java!");
+        // Crear un objeto Scanner para leer la entrada del usuario
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitar al usuario que ingrese su nombre
+        System.out.print("Por favor, ingresa tu nombre: ");
+        String nombre = scanner.nextLine();
+
+        // Imprimir un mensaje personalizado
+        System.out.println("Hola, " + nombre + "! Este es mi programa mejorado en Java.");
+        
+        // Cerrar el scanner para liberar recursos
+        scanner.close();
     }
 }
