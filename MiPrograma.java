ecfd2f64f41fcc69aa26204977869bcca2e06c45
antoniohcarr <ahf0007@alu.medac.es>
2024-05-10 18:50:02 +0200

    Create MiPrograma.java

---
+++ MiPrograma.java
@@ -0,0 +1,7 @@
+public class MiPrograma {
+
+    public static void main(String[] args) {
+        // Imprime un mensaje en la consola
+        System.out.println("Hola, este es mi primer programa en Java!");
+    }
+}
